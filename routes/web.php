38f737e72a551c65fd8c49ecf9ee6b9bb49933fa
tiemtt <tiemtt@gmail.com>
2026-06-01 23:16:33 +0700

    up

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/health', function () {
+    return response()->json([
+        'status' => 'ok',
+        'app' => config('app.name'),
+        'laravel' => app()->version(),
+    ]);
+});
