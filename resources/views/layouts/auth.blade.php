43ec1ce817ac6e7f9f4cddc0828cbdf4b00997b6
tiemtt <tiemtt@gmail.com>
2026-07-06 14:00:43 +0700

    up

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,6 +3,8 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/svg+xml" href="https://www.masterkorean.vn/images/favicon.svg">
+    <link rel="shortcut icon" href="https://www.masterkorean.vn/images/favicon.svg">
     <title>{{ __('lf.LF_auth_login_title') }} - Master Korean</title>
 
     @vite(['resources/css/auth.css', 'resources/js/app.js'])
